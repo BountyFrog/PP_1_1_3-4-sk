ac7c2f0d0777e3be39f803b2edcc4e4eace81e8e
BountyFrog <112285512+BountyFrog@users.noreply.github.com>
2024-05-13 14:14:18 +0300

    load on github

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Alex", "Pushkin", (byte) 28);
+        userService.saveUser("Leo", "Tolstoy", (byte) 43);
+        userService.saveUser("Fyodor", "Dostoevsky", (byte) 35);
+        userService.saveUser("Nickolas", "Gogol", (byte) 47);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
